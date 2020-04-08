e3393ed8221ec88729d88370f4e1eeb60bd40dfc
Your Name <matt-picard@hotmail.fr>
2020-04-08 13:09:12 +0200

    komiit

--- src/app/_services/token.interceptor.ts
+++ src/app/_services/token.interceptor.ts
@@ -1,38 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Component } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { AuthService } from './auth/auth.service';
 import { Observable, throwError, BehaviorSubject } from 'rxjs';
 import { catchError, switchMap, filter, take } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
     private isRefreshing = false;
     private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
-    constructor(public authService: AuthService) { }
+    constructor(public authService: AuthService,private _snackBar: MatSnackBar) { }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    if (this.authService.getJwtToken()) {
-      request = this.addToken(request, this.authService.getJwtToken());
+      if (this.authService.getJwtToken()) {
+        request = this.addToken(request, this.authService.getJwtToken());
+      }
+  
+      return next.handle(request).pipe(catchError(error => {
+        if (error instanceof HttpErrorResponse && error.status === 401) {
+          this._snackBar.open("L'authentification a échouée", "FERMER");
+          return new Observable<HttpEvent<any>>();
+        } else {
+          return new Observable<HttpEvent<any>>();
+        }
+      }));
     }
-
-    return next.handle(request).pipe(catchError(error => {
-      if (error instanceof HttpErrorResponse && error.status === 401) {
-       
-        return throwError(error);
-      }
-    }));
-  }
-  private handle401Error(request: HttpRequest<any>, next: HttpHandler) {
-    return 401;
-  }
-
-  private addToken(request: HttpRequest<any>, token: string) {
-    return request.clone({
-      setHeaders: {
-        'Authorization': `Bearer ${token}`
-      }
-    });
-  }
+  
+    private addToken(request: HttpRequest<any>, token: string) {
+      return request.clone({
+        setHeaders: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+    }
+    
 }
