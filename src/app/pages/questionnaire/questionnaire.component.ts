5662c000e7242c39bd2eb2b9ddf5715a8fdc6e57
dleveneur <davyleveneur@gmail.com>
2020-04-08 15:31:13 +0400

    affichage des questions dans la base

--- src/app/pages/questionnaire/questionnaire.component.ts
+++ src/app/pages/questionnaire/questionnaire.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit,} from '@angular/core';
 import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 import { MaterialModule } from 'app/_services/material.module';
 import { SurveyService } from 'app/_services/survey/survey.service';
+import { SurveyAnswerService } from 'app/_services/surveyAnswer/survey-answer.service';
+import { SurveyChoiceService } from 'app/_services/surveyChoice/survey-choice.service';
+import { Survey } from 'app/_models/Survey';
 
 /**
  * @title Questionnaire
@@ -15,15 +18,23 @@ export class QuestionnaireComponent implements OnInit {
   isLinear = false;
   firstFormGroup:  FormGroup;
   secondFormGroup: FormGroup;
-  
-  constructor(private _formBuilder: FormBuilder, private _SurveyServices: SurveyService) {}
+  listQuestion: Array<Survey>;
+  constructor( private _formBuilder: FormBuilder, private _SurveyServices: SurveyService, 
+               private _SurveyAnswerService: SurveyAnswerService, private _SurveyChoiceService: SurveyChoiceService) {}
 
+  
+  
   ngOnInit() {
+    this._SurveyServices.getSurveys().subscribe(data => {this.listQuestion = data;  console.log(this.listQuestion)  });
     this.firstFormGroup = this._formBuilder.group({
       firstCtrl: ['', Validators.required]
     });
     this.secondFormGroup = this._formBuilder.group({
       secondCtrl: ['', Validators.required]
     });
+  }
+
+  validQuestionnaire(){
+    
   }
 }
