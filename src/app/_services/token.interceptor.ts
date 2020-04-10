95636814a61f3f75a15806727dc4d6ef153b81ca
Your Name <matt-picard@hotmail.fr>
2020-04-10 07:43:13 +0200

    commt

--- src/app/_services/token.interceptor.ts
+++ src/app/_services/token.interceptor.ts
@@ -4,13 +4,15 @@ import { AuthService } from './auth/auth.service';
 import { Observable, throwError, BehaviorSubject } from 'rxjs';
 import { catchError, switchMap, filter, take } from 'rxjs/operators';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { ThrowStmt } from '@angular/compiler';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
     private isRefreshing = false;
     private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
-    constructor(public authService: AuthService,private _snackBar: MatSnackBar) { }
+    constructor(public authService: AuthService,private _snackBar: MatSnackBar,private router:Router) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
@@ -21,6 +23,8 @@ export class TokenInterceptor implements HttpInterceptor {
       return next.handle(request).pipe(catchError(error => {
         if (error instanceof HttpErrorResponse && error.status === 401) {
           this._snackBar.open("L'authentification a échouée", "FERMER");
+          this.authService.logout();
+          this.router.navigate(['/login']);
           return new Observable<HttpEvent<any>>();
         } else {
           return new Observable<HttpEvent<any>>();
